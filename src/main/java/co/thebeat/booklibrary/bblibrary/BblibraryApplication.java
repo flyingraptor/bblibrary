9da511a744503e21204d300b370a619428f5ad15
nraptis <nikos.raptis@thebeat.co>
2018-07-25 20:52:11 +0300

    Initial commit

---
+++ src/main/java/co/thebeat/booklibrary/bblibrary/BblibraryApplication.java
@@ -0,0 +1,12 @@
+package co.thebeat.booklibrary.bblibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BblibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BblibraryApplication.class, args);
+	}
+}
---
+++ src/test/java/co/thebeat/booklibrary/bblibrary/BblibraryApplicationTests.java
@@ -0,0 +1,16 @@
+package co.thebeat.booklibrary.bblibrary;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BblibraryApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
